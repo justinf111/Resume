97498dfa93fe4777e4d940aa808a7c01b7b945c9
Justin Favaloro <justin.favaloro@blueglue.com.au>
2020-07-11 10:13:58 +1000

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{url('/css/app.css')}}">
     </head>
     <body>
